d8801fd2cb291972c7359a0f0880cce1f313dcac
Trillezco <89798807+Trillezco@users.noreply.github.com>
2021-09-02 15:17:51 -0400

    Add files via upload

--- Animator.py
+++ Animator.py
@@ -1,31 +1,7 @@
 import Functions as F
 
 
-# images="/home/gonzatrille/Escritorio/Universidad/Tesis/My Algorithm/Histogram/narrow/centimeter/**"
-# images="/home/gonzatrille/Escritorio/Universidad/Tesis/My Algorithm/Histogram/narrow/parsec/**"
-# images= "/home/gonzatrille/Escritorio/Universidad/Tesis/My Algorithm/Histogram/wide/centimeter/**"
-# images="/home/gonzatrille/Escritorio/Universidad/Tesis/My Algorithm/Histogram/wide/parsec/**"
-# images= "/home/gonzatrille/Escritorio/Universidad/Tesis/My Algorithm/kde/parsec/**"
-# images= "/home/gonzatrille/Escritorio/Universidad/Tesis/My Algorithm/kde/centimeter/**"
-images= "/home/gonzatrille/Escritorio/Universidad/Tesis/My Algorithm/kde/centimeter (copia)/**"
-
 images= "/home/gonzatrille/Escritorio/Universidad/Tesis/My Algorithm/kde/centimeter"
 
-Animator(images)
+F.Animator(images)
 
-files = glob.glob(images)
-files=natsort.natsorted(files)
-
-Image = []
-for File in files:
-	Image.append(imageio.imread(File))
-imageio.mimsave('KDENarrowCentimeterOtro.gif', Image, fps=10, duration=0.2)
-
-
-def Animator(Root):
-	plots = glob.glob(Root+"/**")
-	plots = natsort.natsorted(plots)	;	List = []
-	for Plot in plots:
-		List.append(imageio.imread(Plot))
-	imageio.mimsave("Animation.gif", List, fps=10, duration=0.2)
-	return
